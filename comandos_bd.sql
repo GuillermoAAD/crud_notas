9f42c89c7072e8ad1ae6193c5937abc7f7e05469
mw7225 <memofenton@gmail.com>
2021-07-14 22:24:03 -0500

    se creo BD, se probo conexion BD

---
+++ comandos_bd.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE db_notas;
+
+CREATE TABLE db_notas.usuario
+(id_usuario INT NOT NULL AUTO_INCREMENT , 
+username VARCHAR(20) NOT NULL , 
+pass VARCHAR(20) NOT NULL , 
+PRIMARY KEY (id_usuario) );
+ 
+INSERT INTO usuario
+(id_usuario, username, pass) 
+VALUES (NULL, user1, user1);
+
+CREATE TABLE db_notas.nota
+(id_nota INT NOT NULL AUTO_INCREMENT, 
+titulo VARCHAR(50), 
+descripcion TEXT, 
+fecha DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+id_usuario INT NOT NULL, 
+PRIMARY KEY (id_nota),
+FOREIGN KEY (id_usuario)
+        REFERENCES db_notas.usuario(id_usuario)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION);
+
+INSERT INTO nota
+(id_nota, titulo, descripcion, fecha, id_usuario) 
+VALUES (NULL, 'Nota 1', 'Una nota', current_timestamp(), 1);
+
---
+++ conexion.php
@@ -0,0 +1,16 @@
+  
+<?php
+
+$host = 'localhost';
+$user_bd = 'root';
+$pass_bd = '';
+$bd = 'db_notas';
+ 
+$conn = mysqli_connect($host, $user_bd, $pass_bd, $bd);
+
+if (!$conn) {
+   echo "error al conectar al Servidor de BD";
+}
+
+
+?>
